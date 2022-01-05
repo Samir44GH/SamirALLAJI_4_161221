7733ee2a22c0f27c46e758ffbc6e67a25b390400
ALLAJI <samir27021986@hotmail.fr>
2022-01-05 15:38:33 +0100

    Création d'un message d'erreur pour les radio et les conditions d'utilisations

--- GameOn/starterOnly/modal.js
+++ GameOn/starterOnly/modal.js
@@ -24,6 +24,9 @@ const locationsElt = document.getElementsByName("location");
 const checkbox1Elt = document.querySelector("#checkbox1");
 const checkbox2Elt = document.querySelector("#checkbox2");
 
+//Création d'un élément newError
+let newError = document.createElement("error");
+
 //Validation elements
 const validate = (e) => {
   e.preventDefault();
@@ -37,80 +40,147 @@ const validate = (e) => {
   console.log(e.target.checkbox1.checked);
   console.log(e.target.checkbox2.value);
 };
-
-//Message error validation
-firstElt.addEventListener("focusout", (e) => {
-  console.log(e.target.value.length);
-  if (e.target.value.length < 2) {
-    let newError = document.createElement("error");
+/*
+//Validation du firstElt
+function validateFirsElt() {
+  if (firstElt.value.length < 2) {
+    //Si la valeur retournée est < de 2 caractères => l'utilisatuer sera informé par le message d'erreur
     newError.textContent =
       "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     document.querySelectorAll(".formData")[0].appendChild(newError);
+    //Permet de connaître la validation de l'entrée sur la console
+    console.log(
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    );
+    return false;
+  } else {
+    newError.textContent = "";
+  }
+  return true;
+}
+//Validation du lastElt
+function validateLastElt() {
+  if (lastElt.value.length < 2) {
+    //Si la valeur retournée est < de 2 caractères => l'utilisatuer sera informé par le message d'erreur
+    newError.textContent =
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    document.querySelectorAll(".formData")[1].appendChild(newError);
+    //Permet de connaître la validation de l'entrée sur la console
+    console.log(
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    );
+    return false;
+  } else {
+    newError.textContent = "";
+    return true;
+  }
+}
+//Validation du emailElt
+function validateEmailElt() {
+  //Si la valeur retournée ne correspond pas à la regex => l'utilisateur sera informé par le message d'erreur
+  if (
+    !e.target.email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  ) {
+    newError.textContent = "Veuillez entrer une adresse email valide.";
+    document.querySelectorAll(".formData")[2].appendChild(newError);
+    //Permet de connaître la validation de l'entrée sur la console
+    console.log("Veuillez entrer une adresse email valide.");
+    return false;
+  } else {
+    newError.textContent = "";
+    return true;
+  }
+}*/
+
+//Message error validation
+//Création d'un élément newError
+
+firstElt.addEventListener("focusout", (e) => {
+  //Si la valeur retournée est < de 2 caractères => l'utilisatuer sera informé par le message d'erreur
+  if (e.target.value.length < 2) {
+    newError.textContent =
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+    document.querySelectorAll(".formData")[0].appendChild(newError);
+    //Permet de connaître la validation de l'entrée sur la console
+    console.log(
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    );
   }
 });
 
 lastElt.addEventListener("focusout", (e) => {
   console.log(e.target.value.length);
+  //Si la valeur retournée est < de 2 caractères => l'utilisateur sera informé par le message d'erreur
   if (e.target.value.length < 2) {
-    let newError = document.createElement("error");
     newError.textContent =
       "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     document.querySelectorAll(".formData")[1].appendChild(newError);
+    //Permet de connaître la validation de l'entrée sur la console
+    console.log("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
   }
 });
 
 emailElt.addEventListener("focusout", (e) => {
   console.log(e.target.value);
+  //Si la valeur retournée ne correspond pas à la regex => l'utilisateur sera informé par le message d'erreur
   if (!e.target.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-    let newError = document.createElement("error");
     newError.textContent = "Veuillez entrer une adresse email valide.";
     document.querySelectorAll(".formData")[2].appendChild(newError);
+    //Permet de connaître la validation de l'entrée sur la console
+    console.log("Veuillez entrer une adresse email valide.");
   }
 });
 
 birthdateElt.addEventListener("focusout", (e) => {
   console.log(e.target.value);
+  //Si la valeur retournée ne correspond pas à la regex => l'utilisateur sera informé par le message d'erreur
   if (
     !e.target.value.match(
       /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/
     )
   ) {
-    let newError = document.createElement("error");
-    newError.textContent = "Veuillez indiquer votre date de naissance";
+    newError.textContent = "Veuillez entrer votre date de naissance";
     document.querySelectorAll(".formData")[3].appendChild(newError);
+    //Permet de connaître la validation de l'entrée sur la console
+    console.log("Veuillez entrer votre date de naissance");
   }
 });
 
 quantityElt.addEventListener("focusout", (e) => {
   console.log(e.target.value.match);
+  //Si la valeur retournée ne correspond pas à la regex => l'utilisateur sera informé par le message d'erreur
   if (!e.target.value.match(/^([0-9]){1,2}$/)) {
-    let newError = document.createElement("error");
     newError.textContent = "Veuillez saisir une valeur numérique.";
     document.querySelectorAll(".formData")[4].appendChild(newError);
+    //Permet de connaître la validation de l'entrée sur la console
+    console.log("Veuillez saisir une valeur numérique.");
   }
 });
 
-/*locationElt.addEventListener("focusout", (e) => {
+let checkRadio = document.querySelector('input[name="location"]:checked');
+if (checkRadio !== 0) {
+  console.log("Veuillez saisir une ville.");
+  newError.textContent = "Veuillez saisir une ville.";
+  document.querySelectorAll(".formData")[5].appendChild(newError);
+}
+
+let checkCondition = document.querySelector('input[id="checkbox1"]:checked');
+if (checkCondition !== 0) {
+  console.log("Veuillez accepter les conditions");
+  newError.textContent = "Veuillez accepter les conditions d'utilisation.";
+  document.querySelectorAll(".formData")[6].appendChild(newError);
+}
+/*locationsElt.addEventListener("click", (e) => {
   console.log(e.target.value.match);
-  if (
-    !e.target.value.match ===
-    [
-      location[0].checked == true ||
-        location[1].checked == true ||
-        location[2].checked == true ||
-        location[3].checked == true ||
-        location[4].checked == true ||
-        location[5].checked == true,
-    ]
-  ) {
+
+  if (!e.target.value.match === 0) {
     let newError = document.createElement("error");
     newError.textContent = "Veuillez saisir une ville.";
     document.querySelectorAll(".formData")[5].appendChild(newError);
   }
 });*/
 
-/*locationsElt.addEventListener("focusout", (i) => {
-  console.log(e.target.value.length);
+/*locationsElt.addEventListener("checked", (i) => {
   let array = [];
   for (let i = 0; i < locationsElt.length; i++) {
     if (locationsElt[i].checked === true) {
